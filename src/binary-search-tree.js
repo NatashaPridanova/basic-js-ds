dfcbc372b78f98b817699e06fd7215335a6e24a9
Natallia <natashapridanova17@gmail.com>
2022-05-02 16:52:37 +0300

    feat: implement two methods of bst

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,70 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
 
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    root() {
+        return this.root;
+    }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    add(data) {
+        this.root = addInside(this.root, data);
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        function addInside(node, data) {
+            if (!node) { return new Node(data); }
+            if (node.data === data) { return node; }
+            if (data < node.data) { node.left = addInside(node.left, data); } else {
+                node.right = addInside(node.right, data);
+            }
+            return node;
+        }
+    }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    has(data) {
+        return searchInside(this.root, data);
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+        function searchInside(node, data) {
+            if (!node) { return false; }
+            if (node.data === data) {
+                return true;
+            }
+            return data < node.data ?
+                searchInside(node.left, data) :
+                searchInside(node.right, data);
+        }
 
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    }
 
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    find( /* data */ ) {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+    }
+
+    remove( /* data */ ) {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+    }
+
+    min() {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+    }
+
+    max() {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+    }
 }
 
 module.exports = {
-  BinarySearchTree
+    BinarySearchTree
 };
+const tree = new BinarySearchTree;
